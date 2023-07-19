b99fd23b8a05efa2ab07b985281414bb0f8c7849
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2023-07-19 17:15:43 -0300

    add tests for resource handler

---
+++ src/test/java/br/com/oriedroc/api/resources/exceptions/ResourceExceptionHandlerTest.java
@@ -0,0 +1,59 @@
+package br.com.oriedroc.api.resources.exceptions;
+
+import br.com.oriedroc.api.services.exceptions.DataIntegrityViolationException;
+import br.com.oriedroc.api.services.exceptions.ObjectNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class ResourceExceptionHandlerTest {
+
+    @InjectMocks
+    private ResourceExceptionHandler exceptionHandler;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void whenObjectNotFoundExceptionThenReturnAResponseEntity() {
+        ResponseEntity<StandardError> response = exceptionHandler
+                .objectNotFound(
+                        new ObjectNotFoundException("Object not found"),
+                        new MockHttpServletRequest()
+                );
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals("Object not found", response.getBody().getError());
+        assertEquals(404, response.getBody().getStatusCode());
+    }
+
+    @Test
+    void whenDataIntegrityViolationExceptionThenReturnAResponseEntity() {
+        ResponseEntity<StandardError> response = exceptionHandler
+                .dataIntegrityViolationException(
+                        new DataIntegrityViolationException("E-mail already used"),
+                        new MockHttpServletRequest()
+                );
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals("E-mail already used", response.getBody().getError());
+        assertEquals(400, response.getBody().getStatusCode());
+    }
+}
